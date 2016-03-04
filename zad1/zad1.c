bd08a5dd4746c6a9f2b52fcc73e0309fe73fca33
Kamil Pek <kpek@sigma.ug.edu.pl>
2016-03-04 19:12:10 +0100

    final

--- zad1/zad1.c
+++ zad1/zad1.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-#define liczba_bitow 32                                  // definicja liczby bitow dla funkcji toBinary
+#define liczba_bitow 32                                    // definicja liczby bitow dla funkcji toBinary
 
-void kodowanie(long liczba_dziesietna)                     // zamiana na kod U2(32-bitowy)
+void kodowanie(int liczba_dziesietna)                      // zamiana na kod U2(32-bitowy)
 {
   int i;
-  for(i = 0; i < liczba_bitow; i++) printf("%ld", (liczba_dziesietna >> (liczba_bitow - (i + 1))) & 0x1);
-  printf("\n");
+  for(i = 0; i < liczba_bitow; i++) printf("%d", (liczba_dziesietna >> (liczba_bitow - (i + 1))) & 1);
+  printf("\n");                                            // ^ wyswietlanie po jednym bicie ^
 }
 
 int main(){
   FILE *plik = NULL;
-  plik = fopen("plik.txt", "r");                         // otwarcie pliku tekstowego w trybie czytania
+  plik = fopen("plik.txt", "r");                           // otwarcie pliku tekstowego w trybie czytania
 
-  if (plik == NULL){                                     // sprawdzanie istnienia pliku
+  if (plik == NULL){                                       // sprawdzanie istnienia pliku
     printf("Otwarcie pliku się nie powiodło!\n");
     return 1;
   }
   else {
     int j;
     int liczby = 0;
-    while(1) {                                            // wieczny while
+    while(1) {                                             // wieczny while
       fscanf(plik, "%i", &liczby);                         // wczytywanie liczb z pliku do tablicy "cyfry"
-      if(feof(plik) != 0) break;                          // konczenie petli jak skonczy sie zawartosc pliku
+      if(feof(plik) != 0) break;                           // konczenie petli jak skonczy sie zawartosc pliku
       if(!liczby) {                                        // obsluga niedozwolonych znakow
         printf("W pliku znajduja sie nie dozwolone znaki, prosze popraw to.\n");
         printf("Koncze dzialanie programu.\n\n");
@@ -31,7 +31,7 @@ int main(){
       kodowanie(liczby);
       liczby = 0;
     }
-    fclose(plik);                                         // zamykanie pliku
+    fclose(plik);                                          // zamykanie pliku
   }
   return 0;
 }
