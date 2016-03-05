11b58f7b1c6a97a64dc2739ac807e81ae88ab5ea
Kamil Pek <kamil.pek@gmail.com>
2016-03-05 12:58:43 +0100

    laborki 2.

--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 ## Architektura Komputerów - 2015/2016
 ### Zadania
 
-* Zadanie 1. - Zamiana liczb na kod U2(32-bitowy),
-
+* Zadanie 1. - Zamiana liczb całkowitych zapisanych w systemie dziesiętnym na ich kod U2(32-bitowy);
+* Zadanie 2. - Zamiana liczb zmiennoprzecinkowych w systemie dziesiętnym na ich reprezentację w formacie IEEE 754 na 32-bitach lub 64-bitach;
 
 ---
+(C)Kamil Pek - 2016
--- zad2/zad2.c
+++ zad2/zad2.c
@@ -1,4 +1,4 @@
-// Zamana liczb caÅ‚kowitych zapisanych w systemie dziesiÄ™tnym na ich kod U2.
+// Zamana liczb zmiennoprzecinkowych na kod IEEE 754
 // Autor: Kamil Pek 231050. Data: 04.03.2016.
 
 #include <stdio.h>
@@ -6,9 +6,13 @@
 
 void kodowanie(int liczba_dziesietna)                      // zamiana na kod U2(32-bitowy)
 {
+  FILE *wynik = NULL;
+  wynik = fopen("wynik.txt", "a");
   int i;
-  for(i = 0; i < liczba_bitow; i++) printf("%d", (liczba_dziesietna >> (liczba_bitow - (i + 1))) & 1);
-  printf("\n");                                            // ^ wyswietlanie po jednym bicie ^
+  for(i = 0; i < liczba_bitow; i++) {
+    fprintf(wynik, "%d", (liczba_dziesietna >> (liczba_bitow - (i + 1))) & 1);
+  }
+  fprintf(wynik, "\n");
 }
 
 int main(){
@@ -21,16 +25,15 @@ int main(){
   }
   else {
     int j;
-    int liczby = 0;
+    float liczby = 0;
     while(1) {                                             // wieczny while
-      fscanf(plik, "%i", &liczby);                         // wczytywanie liczb z pliku do tablicy "cyfry"
+      fscanf(plik, "%f", &liczby);                         // wczytywanie liczb z pliku do tablicy "cyfry"
       if(feof(plik) != 0) break;                           // konczenie petli jak skonczy sie zawartosc pliku
       if(!liczby) {                                        // obsluga niedozwolonych znakow
         printf("W pliku znajduja sie nie dozwolone znaki, prosze popraw to.\n");
         printf("Koncze dzialanie programu.\n\n");
         break;
       }
-      printf("%d\t ", liczby);
       kodowanie(liczby);
       liczby = 0;
     }
