1b51ea55c4598884d898c571f91aaa096b7acf02
Kamil Pek <kpek@sigma.ug.edu.pl>
2016-04-28 20:58:07 +0200

    wersja poprawiona

--- zad6/zad6.asm
+++ zad6/zad6.asm
@@ -1,108 +1,125 @@
 .model small
 .386
 .data
-	bufor db 0
+	bufor dd 0
 	msg_dzie db "d=","$"
 	msg_dwoj db "b=","$"
 	msg_osem db "o","$"
 .stack 100h
 .code
 start:
-	mov ax, @data
-	mov ds, ax
-	mov ax, 0	
-	mov al, 234
-	mov bufor, al
-	
-	mov cx, 3
-	dziesietny_licz:					; dziesietny
-		mov bh, 10
-		div bh
-		mov bl, ah	
-		add bl, 30h
-		mov dl, bl
-		push bx
-		mov dl, al
-		mov ax, 0
-		mov al, dl
-		dec cx
-		jnz dziesietny_licz
-	
-	mov cx, 3
-	dziesietny_wysw:	
-		mov ax, 0
-		pop bx
-		mov al, bl
-		mov ah, 0Eh
-		int 10h
-		dec cx
-		jnz dziesietny_wysw		
-
-	mov dx, offset msg_dzie
-	mov ah, 09h
-	int 21h	
-	mov ax, 0
-	mov al, bufor
-	
-	mov cx, 8
-	dwojkowy_licz:						; dwojkowy
-		mov bh, 2
-		div bh
-		mov bl, ah	
-		add bl, 30h
-		mov dl, bl
-		push bx
-		mov dl, al
-		mov ax, 0
-		mov al, dl
-		dec cx
-		jnz dwojkowy_licz
+	mov eax, @data
+	mov ds, eax
+	mov eax, 0
+	mov eax, 65536	; 65536 / 234
+	mov bufor, eax
 		
-	mov cx, 8
-	dwojkowy_wysw:	
-		mov ax, 0
-		pop bx
-		mov al, bl
-		mov ah, 0Eh
-		int 10h
-		dec cx
-		jnz dwojkowy_wysw		
+	dziesietny_licz:					; dziesietny
+		mov eax, bufor
+		mov esp, 0
+		
+		dziesietny_licz_petla:
+			mov ebx, 0
+			mov edx, 0			
+			mov bl, 10
+			div ebx					
+			add edx, 30h			
+			push edx			
+			cmp eax, 0
+			je dziesietny_wysw
+		jmp dziesietny_licz_petla	
+			
+	dziesietny_wysw:
 	
-	mov dx, offset msg_dwoj
-	mov ah, 09h
-	int 21h	
-	mov ax, 0	
-	mov al, bufor
+		dziesietny_wysw_petla:
+			cmp esp, 0
+			je dziesietny_wysw_znak
+			pop edx
+			mov ah, 0Eh
+			mov al, dl
+			int 10h
+			jmp dziesietny_wysw_petla
 	
-	mov cx, 3
-	osemkowy_licz:					; ósemkowy
-		mov bh, 8
-		div bh
-		mov bl, ah	
-		add bl, 30h
-		mov dl, bl
-		push bx
-		mov dl, al
-		mov ax, 0
-		mov al, dl
-		dec cx
-		jnz osemkowy_licz
+		dziesietny_wysw_znak:
+			mov dx, offset msg_dzie
+			mov ah, 09h
+			int 21h	
+			mov eax, 0
+			mov eax, bufor
 	
-	mov cx, 3
-	osemkowy_wysw:	
-		mov ax, 0
-		pop bx
-		mov al, bl
-		mov ah, 0Eh
-		int 10h
-		dec cx
-		jnz osemkowy_wysw
+	jmp dwojkowy
 	
-	mov dx, offset msg_osem
-	mov ah, 09h
-	int 21h	
-	mov ax, 0
-	mov al, bufor
+	dwojkowy:
+	
+	dwojkowy_licz:					; dwojkowy
+		mov eax, bufor
+		mov esp, 0
+		
+		dwojkowy_licz_petla:
+			mov ebx, 0
+			mov edx, 0			
+			mov bl, 2
+			div ebx					
+			add edx, 30h			
+			push edx			
+			cmp eax, 0
+			je dwojkowy_wysw
+		jmp dwojkowy_licz_petla	
+			
+	dwojkowy_wysw:
+	
+		dwojkowy_wysw_petla:
+			cmp esp, 0
+			je dwojkowy_wysw_znak
+			pop edx
+			mov ah, 0Eh
+			mov al, dl
+			int 10h
+			jmp dwojkowy_wysw_petla
+	
+		dwojkowy_wysw_znak:
+			mov dx, offset msg_dwoj
+			mov ah, 09h
+			int 21h	
+			mov eax, 0
+			mov eax, bufor
+	
+	jmp osemkowy
+	
+	osemkowy:
+	
+	osemkowy_licz:					; osemkowy
+		mov eax, bufor
+		mov esp, 0
+		
+		osemkowy_licz_petla:
+			mov ebx, 0
+			mov edx, 0			
+			mov bl, 8
+			div ebx					
+			add edx, 30h			
+			push edx			
+			cmp eax, 0
+			je osemkowy_wysw
+		jmp osemkowy_licz_petla	
+			
+	osemkowy_wysw:
+	
+		osemkowy_wysw_petla:
+			cmp esp, 0
+			je osemkowy_wysw_znak
+			pop edx
+			mov ah, 0Eh
+			mov al, dl
+			int 10h
+			jmp osemkowy_wysw_petla
+	
+		osemkowy_wysw_znak:
+			mov dx, offset msg_osem
+			mov ah, 09h
+			int 21h	
+			mov eax, 0
+			mov eax, bufor
 	
 	mov ah, 4Ch
 	int 21h  	
